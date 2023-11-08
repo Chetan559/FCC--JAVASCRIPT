9da28a380cc8a8219235ee7af2cf0aa97d6a55b6
Chetan Sharma <cschetan559@gmail.com>
2023-11-08 19:17:12 +0530

    revising the concepts of JS

---
+++ basicJS.js
@@ -0,0 +1,43 @@
+// local scope and functions
+function myLocalScope() {
+    // Only change code below this line
+    const myVar = 1; 
+    console.log('inside myLocalScope', myVar);
+  }
+  myLocalScope();
+  
+  // Run and check the console
+  // myVar is not defined outside of myLocalScope
+  console.log('outside myLocalScope', myVar);
+
+//   Global vs. Local Scope in Functions
+// Setup
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+
+const outerWear = "sweater";
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
+// Understanding Undefined Value returned from a Function
+// Setup
+let sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive(){
+  sum = sum + 5;
+}
+
+// Only change code above this line
+
+addThree();
+addFive();
+// Assignment with a Returned Value
