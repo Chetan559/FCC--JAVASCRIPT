4fd897e6fd6ecfad6f80c3cafd043876df568665
Chetan Sharma <cschetan559@gmail.com>
2023-11-17 13:18:31 +0530

    data structure in  JS (3)

--- basicDataStructureJS.js
+++ basicDataStructureJS.js
@@ -118,4 +118,189 @@ function golfScore(par, strokes) {
 
 golfScore(5, 4);
 // Selecting from Many Options with Switch Statements
+function caseInSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+switch (val){
+  case 1:
+   answer = "alpha";
+   break;
+  case 2:
+   answer = "beta";
+   break;
+  case 3:
+   answer = "gamma";
+   break;
+  case 4:
+   answer = "delta";
+   break;
+}
+
+
+  // Only change code above this line
+  return answer;
+}
+
+caseInSwitch(1);
+// Adding a Default Option in Switch Statements
+  /*n a switch statement you may not be able to specify all possible values as case statements. Instead, you can add the default statement which will be executed if no matching case statements are found. Think of it like the final else statement in an if/else chain.*/
+  function switchOfStuff(val) {
+    let answer = "";
+    // Only change code below this line
+     switch(val) {
+       case "a" :
+        return "apple";
+        break;
+       case "b" :
+        return "bird";
+        break;
+       case "c" :
+        return "cat";
+        break;
+       default:
+        return "stuff";
+        break;
+  
+     }
+  
+  
+    // Only change code above this line
+    return answer;
+  }
+  
+  switchOfStuff(1);
+  // Multiple Identical Options in Switch Statements
+  function sequentialSizes(val) {
+    let answer = "";
+    // Only change code below this line
+    switch (val){
+      case 1:
+      case 2:
+      case 3:
+        return "Low";
+        break;
+      case 4:
+      case 5:
+      case 6:
+        return "Mid";
+        break;
+      case 7:
+      case 8:
+      case 9:
+        return "High";
+        break;
+    }
+  
+  
+    // Only change code above this line
+    return answer;
+  }
+  
+  sequentialSizes(1);
+  // Replacing If Else Chains with Switch 
+  function chainToSwitch(val) {
+    let answer = "";
+    // Only change code below this line
+     switch (val){
+       case "bob":
+      answer = "Marley";
+      break;
+       case 42:
+      answer = "The Answer";
+      break;
+       case 1: 
+      answer = "There is no #1";
+      break;
+       case 99:
+      answer = "Missed me by this much!";
+      break;
+       case 7:
+      answer = "Ate Nine";
+      break;
+      } 
+    // if (val === "bob") {
+    //   answer = "Marley";
+    // } else if (val === 42) {
+    //   answer = "The Answer";
+    // } else if (val === 1) {
+    //   answer = "There is no #1";
+    // } else if (val === 99) {
+    //   answer = "Missed me by this much!";
+    // } else if (val === 7) {
+    //   answer = "Ate Nine";
+    // }
+  
+    // Only change code above this line
+    return answer;
+  }
+  
+  chainToSwitch(7);
+  // Returning Boolean Values from Functions
+  function isLess(a, b) {
+    // Only change code below this line
+    // if (a < b) {
+    //   return true;
+    // } else {
+    //   return false;
+    // }
+    return a<b;
+    // Only change code above this line
+  }
+  
+  isLess(10, 15);
+  // Return Early Pattern for Functions
+  // Setup
+function abTest(a, b) {
+  // Only change code below this line
+    if( a< 0 || b<0){
+      return undefined ;
+    }
+
+
+  // Only change code above this line
+
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+abTest(2,2);
+// Counting Cards 
+let count = 0;
+
+function cc(card) {
+  // Only change code below this line
+  switch (card){
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+     count++;
+      break;
+    case 7:
+    case 8:
+    case 9:
+      count = count;
+      break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count--;
+      break
+    
+  }
+  let answer = "";
+   if(count > 0){
+     answer ="Bet";
+   }else {
+     answer = "Hold"
+   } 
+   return count +" "+ answer;
+  // Only change code above this line
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
+// Build JavaScript Objects
+
 
